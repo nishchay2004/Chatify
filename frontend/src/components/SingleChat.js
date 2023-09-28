8c95c85896cb2fa3ca502487be9a33760bacd9d0
nishchay2004 <118045617+nishchay2004@users.noreply.github.com>
2023-09-28 16:39:30 +0530

    remove unwanted dependencies

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -9,9 +9,7 @@ import axios from "axios";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import ProfileModal from "./miscellaneous/ProfileModal";
 import ScrollableChat from "./ScrollableChat";
-import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
-
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
